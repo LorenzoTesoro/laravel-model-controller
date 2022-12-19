89799cb6228ddef7925e2e0ec84e5c69847dea57
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-12-19 15:33:08 +0100

    add movies details

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Lara-model-controller @yield('title', 'Welcome')</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+
+    <header>
+        header
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+    <!-- /.site_main -->
+
+    <footer id="site_footer">
+        footer
+    </footer>
+    <!-- /#site_footer -->
+
+</body>
+
+</html>
